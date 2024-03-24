7fabd4104874841253bbedb1ff86f27490e5d355
Jishan Ahamed <jjahamed33@gmail.com>
2024-03-25 00:25:00 +0530

    header tweaks

--- LearnOpenGL_PDF/src/Main.cpp
+++ LearnOpenGL_PDF/src/Main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include "vendor/stb_image.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
 
 #include "Shader.h"
 
