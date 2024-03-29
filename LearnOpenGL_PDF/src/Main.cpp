f08f0cb1ea9921447dba81dc7d386a7712ed8927
Jishan Ahamed <jjahamed33@gmail.com>
2024-03-30 02:43:56 +0530

    Background color tweaked

--- LearnOpenGL_PDF/src/Main.cpp
+++ LearnOpenGL_PDF/src/Main.cpp
@@ -224,7 +224,7 @@ int main()
 
 		// Render
 		// ------
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Bind textures on corresponding texture units
