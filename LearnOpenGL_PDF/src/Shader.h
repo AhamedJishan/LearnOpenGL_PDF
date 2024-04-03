06a29ff843fe3d1bc9c8cf9ddee38a7613ec3119
Jishan Ahamed <jjahamed33@gmail.com>
2024-04-03 23:30:14 +0530

    SetVec3 overloaded to accept glm::vec3

--- LearnOpenGL_PDF/src/Shader.h
+++ LearnOpenGL_PDF/src/Shader.h
@@ -117,6 +117,10 @@ public:
 	{
 		glUniform3f(glGetUniformLocation(ID, name.c_str()), v1, v2, v3);
 	}
+	void SetVec3(const std::string& name, glm::vec3 value) const
+	{
+		glUniform3f(glGetUniformLocation(ID, name.c_str()), value.x, value.y, value.z);
+	}
 	void SetMat4(const std::string& name, glm::mat4& mat)
 	{
 		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
