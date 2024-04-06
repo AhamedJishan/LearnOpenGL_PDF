a592dd0528a9447640e5da8a7260cd18fadd78bd
Jishan Ahamed <jjahamed33@gmail.com>
2024-04-06 23:05:12 +0530

    light.diffuse values tweaked

--- LearnOpenGL_PDF/src/Main.cpp
+++ LearnOpenGL_PDF/src/Main.cpp
@@ -237,7 +237,7 @@ int main()
 		shader.SetVec3("viewPos", camera.Position);
 
 		shader.SetVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-		shader.SetVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+		shader.SetVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
 		shader.SetVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
 		shader.SetFloat("light.constant", 1.0f);
