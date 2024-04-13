6644647888535dd7cf7bcf9fc3433f898fbed0c1
Jishan Ahamed <jjahamed33@gmail.com>
2024-04-13 23:10:15 +0530

    Model class continued

--- LearnOpenGL_PDF/src/Model.h
+++ LearnOpenGL_PDF/src/Model.h
@@ -17,14 +17,40 @@ public:
 	{
 		LoadModel(path);
 	}
-	void Draw(Shader);
+	void Draw(Shader &shader)
+	{
+		for (unsigned int i = 0; i < meshes.size(); i++)
+		{
+			meshes[i].Draw(shader);
+		}
+	}
 private:
 	// Model Datas
 	std::vector<Mesh> meshes;
 	std::string directory;
 
-	void LoadModel(std::string path);
-	void ProcessNode(aiNode *node, const aiScene *scene);
-	Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene);
+	void LoadModel(std::string path)
+	{
+		Assimp::Importer importer;
+		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+		if (!scene || scene->mFlags && AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+		{
+			std::cout << "ERROR::ASSIMP: " << importer.GetErrorString() << std::endl;
+			return;
+		}
+
+		directory = path.substr(0, path.find_last_of('/'));
+		ProcessNode(scene->mRootNode, scene);
+	}
+
+	void ProcessNode(aiNode* node, const aiScene* scene)
+	{
+		for (unsigned int i = 0; i < node->mNumMeshes; i++)
+		{
+		}
+	}
+
+	Mesh ProcessMesh(aiMesh* mesh, const aiScene *scene);
 	std::vector<Texture> LoadMaterialTexture(aiMaterial *mat, aiTextureType type, std::string typeName);
 };
