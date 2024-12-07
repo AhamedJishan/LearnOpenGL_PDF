e7f20e3d741e7bcb50897b6ace8ab394c00c4c25
Jishan Ahamed <jjahamed33@gmail.com>
2024-12-08 01:30:46 +0530

    minor tweaks

--- LearnOpenGL_PDF/src/Camera.h
+++ LearnOpenGL_PDF/src/Camera.h
@@ -21,7 +21,7 @@ enum Camera_Movement
 //Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 2.5f;
+const float SPEED = 1.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
 
@@ -100,7 +100,7 @@ public:
 		}
 		if (input == SHIFT)
 		{
-			MovementSpeed = SPEED/10.0f;
+			MovementSpeed = SPEED * 5.0f;
 		}
 		else MovementSpeed = SPEED;
 	}
